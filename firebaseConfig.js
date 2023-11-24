bf079a850487e073d818fa98f018ba3a89eb9aba
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-11-24 11:39:34 +0200

    feat: add firebase config and firebase init

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAINVYJ0Csm4gqzZS5Plvc-nKh2TrHPHYc",
+  authDomain: "expensetracker-5733b.firebaseapp.com",
+  projectId: "expensetracker-5733b",
+  storageBucket: "expensetracker-5733b.appspot.com",
+  messagingSenderId: "494947824052",
+  appId: "1:494947824052:web:e4641bdbb6318cf39bc2e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
